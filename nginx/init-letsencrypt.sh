7069c70ccfe6105bf47ce72847abedcd3ffc0b11
ToπSenpai <15636011+TopiSenpai@users.noreply.github.com>
2020-07-28 13:53:22 +0200

    Update init-letsencrypt.sh

--- nginx/init-letsencrypt.sh
+++ nginx/init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(anteiku.de www.anteiku.de kittybot.anteiku.de ci.anteiku.de api.anteiku.de dynmap.anteiku.de portainer.anteiku.de warehouse.anteiku.de teilelager.anteiku.de)
+domains=(anteiku.de *.anteiku.de)
 rsa_key_size=4096
 data_path="./.data/certbot"
 email="" # Adding a valid address is strongly recommended
