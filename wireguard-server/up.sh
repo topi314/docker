4b2d4890dbb599bd9e148f7b53a13065fdc83f41
TopiSenpai <tobi@strasserhof.com>
2022-12-16 18:12:37 +0100

    fix minecraft server ip

--- wireguard-server/up.sh
+++ wireguard-server/up.sh
@@ -8,7 +8,7 @@ iptables -t nat -I POSTROUTING -p tcp --dport 80 -d 192.168.180.3 -j MASQUERADE
 iptables -t nat -I PREROUTING -p tcp --dport 3025 -j DNAT --to-destination 192.168.180.4
 iptables -t nat -I POSTROUTING -p tcp --dport 3025 -d 192.168.180.4 -j MASQUERADE
 
-# 192.168.180.3 = minecraft
+# 192.168.180.5 = minecraft
 iptables -t nat -I PREROUTING -p tcp --dport 25565 -j DNAT --to-destination 192.168.180.5
 iptables -t nat -I POSTROUTING -p tcp --dport 25565 -d 192.168.180.5 -j MASQUERADE
 
