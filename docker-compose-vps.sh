5e2d7b9b2507688510835279f58f56c5c07c68d8
TopiSenpai <tobi@strasserhof.com>
2023-01-25 03:33:12 +0100

    fixed docker-compose-vp.sh

--- docker-compose-vps.sh
+++ docker-compose-vps.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 export PARAMS=${@}
-find . -maxdepth 1 -mindepth 1 -type d \( -iname "mailserver" ! -iname "wireguard-server" \) -print0 | xargs -0 -I{} sh -c 'cd "{}" && docker compose ${PARAMS}'
+echo -e './mailserver\0./wireguard-server\0' | xargs -0 -I{} sh -c 'cd "{}" && docker compose ${PARAMS}'
