79427b1e4f6b3def49333781ff54fc2815f7c0af
Toπ <git@topi.wtf>
2023-06-12 21:18:10 +0200

    remove pve stuff

--- docker-compose-nas.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-export PARAMS=${@}
-find . -maxdepth 1 -mindepth 1 -type d \( -iname "*" ! -iname "wireguard-server" ! -iname "mailserver" ! -name ".git" \) -print0 | xargs -0 -I{} sh -c 'cd "{}" && docker compose ${PARAMS}'
